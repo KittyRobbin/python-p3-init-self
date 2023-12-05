45c29f3338f8739209ca42af17da1e4d2a7c92fe
Elizabeth <elizabeth.muthoni@student.moringaschool.com>
2023-12-05 11:24:17 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+# Creating a Dog instance with specific name and breed
+my_dog = Dog("Buddy", "Labrador")
+
+# Creating a Dog instance with only a name (defaults breed to "Mutt")
+another_dog = Dog("Max")
+
+print(my_dog.name)  # Output: Buddy
+print(my_dog.breed)  # Output: Labrador
+
+print(another_dog.name)  # Output: Max
+print(another_dog.breed)  # Output: Mutt
